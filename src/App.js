6294878b3410603270f21ec8b9fb199d782531e8
Alex <olivesargreek@gmail.com>
2022-07-16 12:53:03 +0300

    create: Footer component, feat hygen to create React.components, fix basic App structure

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import { Routes, Route, Link } from 'react-router-dom';
-import Header from './components/Header/Header';
-import MainPage from './Pages/Main/MainPage';
-import EventsPage from './Pages/EventsPage/EventsPage';
-import AboutPage from './Pages/AboutUsPage/AboutUsPage';
-import LocationsPage from './Pages/LocationsPage/LocationsPage';
-import RoutesPage from './Pages/RoutesPage/RoutesPage';
-import NotFoundPage from './Pages/NotFoundPage/NotFoundPage';
-import Modal from './components/Modal/Modal';
-import Footer from './components/Footer/Footer';
+import Header from './components/Header';
+// import MainPage from './Pages/Main/MainPage';
+// import EventsPage from './Pages/EventsPage/EventsPage';
+// import AboutPage from './Pages/AboutUsPage/AboutUsPage';
+// import LocationsPage from './Pages/LocationsPage/LocationsPage';
+// import RoutesPage from './Pages/RoutesPage/RoutesPage';
+// import NotFoundPage from './Pages/NotFoundPage/NotFoundPage';
+// import Modal from './components/Modal/Modal';
+import Footer from './components/Footer';
 import './App.scss';
 import { useState } from 'react';
 
 function App() {
 
-  const [modalActive, setModalActive] = useState(true);
+  // const [modalActive, setModalActive] = useState(true);
 
   return (
-    <div className="App">
-      <Header />
-      <Routes>
+      <>
+        <Header />
+        {/* <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/events' element={<EventsPage />} />
         <Route path='/locations' element={<LocationsPage />} />
         <Route path='/routes' element={<RoutesPage />} />
         <Route path='/aboutUs' element={<AboutPage />} />        
         <Route path='*' element={<NotFoundPage />} />
-      </Routes>
+      </Routes>  */}
       <Footer />
-    </div>
+      
+    
+      </>
+     
+    
+  
   );
 }
 
